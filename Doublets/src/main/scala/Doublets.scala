6060d639de29a30e7f38181815d4a82eda548ea5
Jakub Szwachła <meszkax@gmail.com>
2018-03-21 22:22:15 +0100

    Implement doublets as breadth-first search

--- Doublets/src/main/scala/Doublets.scala
+++ Doublets/src/main/scala/Doublets.scala
@@ -1,21 +1,28 @@
 import scala.io.Source
 
 object Doublets {
-  val dictionary = Source.fromResource("words.txt").getLines.toSet
+  val dictionary = Source.fromResource("words.txt").getLines.toSeq
 
-  def changeLetter(word1: String, word2: String, n: Int): String = {
-    word1.substring(0, n) + word2(n) + word1.substring(n + 1)
+  def distance(word1: String, word2: String): Int =
+    (word1, word2).zipped.map(_ == _).count(_ == false)
+
+  def neighboors(word: String): Seq[String] =
+    dictionary.filter(_.length == word.length).filter(distance(_, word) == 1)
+
+  def bfs(start: String, target: String): List[String] = {
+    def aux(queue: List[List[String]]): List[String] = {
+      val path = queue.head
+      val current  = path.head
+      if (current == target)
+        path.reverse
+      else
+        aux(queue.tail ++ neighboors(current).map(_ :: path))
+    }
+    aux(List(List(start)))
   }
 
-  def nextLink(word1: String, word2: String): String = {
-    val candidates =
-      for (i <- 0 until word1.length) yield changeLetter(word1, word2, i)
-    candidates.filter(_ != word1).find(word => dictionary.contains(word)).get
-  }
-
-  def doublets(word1: String, word2: String): Seq[String] = {
-    ???
-  }
+  def doublets(word1: String, word2: String): Seq[String] =
+    bfs(word1, word2)
 
   def main(args: Array[String]): Unit = println(dictionary)
 }
