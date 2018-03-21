8db8933fa17deaa0e27c7ae0cb9582820bb17826
Jakub Szwachła <meszkax@gmail.com>
2018-03-21 23:53:44 +0100

    Optimize neighboors

--- Doublets/src/main/scala/Doublets.scala
+++ Doublets/src/main/scala/Doublets.scala
@@ -1,15 +1,15 @@
 import scala.io.Source
 
 object Doublets {
-  val dictionary = Source.fromResource("words_big.txt").getLines.toSeq
+  val fullDictionary = Source.fromResource("words_big.txt").getLines.toSeq
 
   def distance(word1: String, word2: String): Int =
     (word1, word2).zipped.map(_ == _).count(_ == false)
 
-  def neighboors(word: String): Seq[String] =
-    dictionary.filter(_.length == word.length).filter(distance(_, word) == 1)
-
   def bfs(start: String, target: String): List[String] = {
+    val dictionary = fullDictionary.filter(_.length == start.length)
+    def neighboors(word: String): Seq[String] =
+      dictionary.filter(distance(_, word) == 1)
     def aux(queue: List[List[String]]): List[String] = {
       if (queue.isEmpty)
         List()
@@ -28,5 +28,5 @@ object Doublets {
   def doublets(word1: String, word2: String): Seq[String] =
     bfs(word1, word2)
 
-  def main(args: Array[String]): Unit = println(dictionary)
+  def main(args: Array[String]): Unit = println(fullDictionary)
 }
