37291c57c7b3b304dabf76ba1f78219363520020
Jakub Szwachła <meszkax@gmail.com>
2018-03-26 20:43:41 +0200

    Implement bidirectional BFS

--- Doublets/src/main/scala/Doublets.scala
+++ Doublets/src/main/scala/Doublets.scala
@@ -31,8 +31,49 @@ object Doublets {
     aux(List(List(start)), Set(), 0)
   }
 
+  def bidirectionalBFS(start: String, target: String): List[String] = {
+    val dictionary = fullDictionary.filter(_.length == start.length)
+    def neighboors(word: String): Seq[String] =
+      dictionary.filter(distance(_, word) == 1)
+    def aux(leftQueue: List[List[String]],
+            rightQueue: List[List[String]],
+            leftSeen: Set[String],
+            rightSeen: Set[String],
+            steps: Int): List[String] = {
+      if (leftQueue.isEmpty || rightQueue.isEmpty)
+        List()
+      else {
+        val leftPath = leftQueue.head
+        val leftCurrent = leftPath.head
+        val rightPath = rightQueue.head
+        val rightCurrent = rightPath.head
+        if (rightSeen.contains(leftCurrent)) {
+          println("steps: " + steps)
+          leftPath.tail.reverse ++ rightQueue.find(_.head == leftCurrent).get
+        }
+        else if (leftSeen.contains(rightCurrent)) {
+          println("steps: " + steps)
+          leftQueue.find(_.head == rightCurrent).get.reverse ++ rightPath.tail
+        }
+        else {
+          val leftUnseenNeighboors = neighboors(leftCurrent).filterNot(leftSeen.contains)
+          val rightUnseenNeighboors = neighboors(rightCurrent).filterNot(rightSeen.contains)
+          aux(leftQueue.tail ++ leftUnseenNeighboors.map(_ :: leftPath),
+              rightQueue.tail ++ rightUnseenNeighboors.map(_ :: rightPath),
+              leftSeen ++ leftUnseenNeighboors,
+              rightSeen ++ rightUnseenNeighboors,
+              steps + 2)
+        }
+      }
+    }
+    if (start.length != target.length)
+      List()
+    else
+      aux(List(List(start)), List(List(target)), Set(), Set(), 0)
+  }
+
   def doublets(word1: String, word2: String): Seq[String] =
-    bfs(word1, word2)
+    bidirectionalBFS(word1, word2)
 
   def main(args: Array[String]): Unit = println(fullDictionary)
 }
